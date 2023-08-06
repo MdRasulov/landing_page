766bb5ab6be6d200e2d9331fe822e254b04317e1
Bubble Ninja <mr.slygrin@gmail.com>
2023-08-06 13:55:52 +0500

    add carousel, form input animation, new styles

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,129 @@
 const burgerBtn = document.querySelector('#hamburger-btn')
-const burgerNav = document.querySelector('#humburder-nav')
+// const mobileNavModal = document.querySelector('#mobile-nav-modal')
+const mobileNavMenu = document.querySelector('#mobile-nav-menu')
 const burgerIcon = document.querySelector('#btn-burger-icon')
 const crossIcon = document.querySelector('#btn-cross-icon')
 
+let swiperIsActive = false
+let swiperServices = null
+
+const swiperUpdates = new Swiper('.swiper-updates', {
+  grabCursor: true,
+  rewind: true,
+
+  navigation: {
+    enabled: false,
+    nextEl: '.swiper-updates-button-next',
+    prevEl: '.swiper-updates-button-prev',
+  },
+
+  pagination: {
+    enabled: true,
+    el: '.swiper-pagination',
+  },
+
+  breakpoints: {
+    576: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 0,
+      navigation: {
+        enabled: true,
+      },
+      pagination: {
+        enabled: false,
+      },
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 0,
+      navigation: {
+        enabled: true,
+      },
+      pagination: {
+        enabled: false,
+      },
+    },
+    1280: {
+      slidesPerView: 4,
+      spaceBetween: 0,
+      navigation: {
+        enabled: true,
+      },
+      pagination: {
+        enabled: false,
+      },
+    },
+  },
+})
+
+const swiperRecommendations = new Swiper('.swiper-recommendations', {
+  grabCursor: true,
+  rewind: true,
+  spaceBetween: 20,
+
+  navigation: {
+    enabled: false,
+    nextEl: '.swiper-recommendations-button-next',
+    prevEl: '.swiper-recommendations-button-prev',
+  },
+
+  pagination: {
+    enabled: true,
+    el: '.swiper-pagination',
+  },
+
+  breakpoints: {
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 0,
+      navigation: {
+        enabled: true,
+      },
+      pagination: {
+        enabled: false,
+      },
+    },
+    1280: {
+      slidesPerView: 3,
+      spaceBetween: 0,
+      navigation: {
+        enabled: true,
+      },
+      pagination: {
+        enabled: false,
+      },
+    },
+  },
+})
+
+const swiperServiseToggle = () => {
+  if (window.innerWidth < 576 && !swiperIsActive) {
+    swiperIsActive = true
+    swiperServices = new Swiper('.swiper-services', {
+      grabCursor: true,
+      rewind: true,
+      spaceBetween: 20,
+
+      pagination: {
+        enabled: true,
+        el: '.swiper-pagination',
+      },
+    })
+  } else if (window.innerWidth > 576 && swiperIsActive) {
+    swiperIsActive = false
+    swiperServices.destroy()
+  }
+}
+swiperServiseToggle()
+
 burgerBtn.addEventListener('click', () => {
-  burgerNav.classList.toggle('hidden')
+  mobileNavMenu.classList.toggle('hidden')
   burgerIcon.classList.toggle('hidden')
   crossIcon.classList.toggle('hidden')
 })
+
+window.addEventListener('resize', swiperServiseToggle)
